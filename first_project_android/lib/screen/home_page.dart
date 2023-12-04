4e88e04dc9bfd3aa97c95226dd0a4b6280da96b4
Dolma <dolma1918@gmail.com>
2023-12-04 17:05:36 +0545

    feat: Separate widget folder created and placed widgets

--- first_project_android/lib/screen/home_page.dart
+++ first_project_android/lib/screen/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:first_project_android/widget/styled_text.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -9,26 +10,29 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Flutter Demo"),
       ),
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color.fromARGB(255, 87, 59, 151),
-              Color.fromARGB(255, 85, 49, 134)
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
+      body: const GradientContainer(),
+    );
+  }
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 87, 59, 151),
+            Color.fromARGB(255, 85, 49, 134)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
         ),
-        child: const Center(
-          child: Text(
-            "Hello world",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 30,
-            ),
-          ),
-        ),
+      ),
+      child: const Center(
+        child: StyledText(),
       ),
     );
   }
